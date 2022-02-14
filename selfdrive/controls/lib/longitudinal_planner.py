7924a307467c05661b38681f1c85e6affb49d8c1
programanichiro <cxk01765@nifty.com>
2022-02-14 16:09:26 +0900

    v_desired_randのセット値を大きくしてみる。

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -222,12 +222,12 @@ class Planner:
         fp.write("%s\n%s\n%s" % (msc ,msl ,msv))
 
     v_desired_rand = 0 #低速の時わざと揺らしてみる。
-    if v_ego < 41 / 3.6:
-      v_desired_rand = random.random() * 0.1 / 3.6
+    if v_ego < 41 / 3.6 and v_ego > 0:
+      v_desired_rand = random.random() * 1.0 / 3.6
       v_desired_rand *= v_ego / 41/3.6
     
     with open('./debug_out_vd','w') as fp:
-      fp.write('v:%.2f , vd:%.2f[km/h] ; vr:%.2f , ad:%.2f[m/ss]' % (v_ego * 3.6 , self.v_desired* 3.6 , v_desired_rand , self.a_desired) )
+      fp.write('v:%.2f , vd:%.2f[km/h] ; vr:%.2f , ad:%.2f[m/ss]' % (v_ego * 3.6 , self.v_desired* 3.6 , v_desired_rand * 3.6 , self.a_desired) )
 
     accel_limits = [A_CRUISE_MIN, get_max_accel(v_ego)]
     #accel_limits_turns = limit_accel_in_turns(v_ego, sm['carState'].steeringAngleDeg, accel_limits, self.CP)
