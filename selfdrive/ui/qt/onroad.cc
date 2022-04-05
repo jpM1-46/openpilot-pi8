e0267ba4fcdc2617e527eb5f414e7549a6a3479f
川岡一郎Max <>
2022-04-05 22:49:08 +0900

    今のところこれしかないか？

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -222,7 +222,7 @@ ButtonsWindow::ButtonsWindow(QWidget *parent) : QWidget(parent) {
 
   btns_layout0->addWidget(btns_wrapperL,0,Qt::AlignVCenter);
 
-  const float all_opac = 0.2;
+  //const float all_opac = 0.2;
   {
     // Handle Ctrl button
     uiState()->scene.mHandleCtrlButton = mHandleCtrlButton = getButtonEnabled("../manager/handle_ctrl_disable.txt");
@@ -232,8 +232,8 @@ ButtonsWindow::ButtonsWindow(QWidget *parent) : QWidget(parent) {
     });
     handleCtrlButton->setFixedWidth(150);
     handleCtrlButton->setFixedHeight(150);
-    handleCtrlButton->setPalette(QColor(255,255,255,all_opac*255));
-    handleCtrlButton->setAutoFillBackground(true);
+    //handleCtrlButton->setPalette(QColor(255,255,255,all_opac*255));
+    //handleCtrlButton->setAutoFillBackground(true);
     //btns_layoutL->addSpacing(70);
     btns_layoutL->addWidget(handleCtrlButton);
     handleCtrlButton->setStyleSheet(QString(btn_style).arg(mButtonColors.at(mHandleCtrlButton)));
@@ -248,7 +248,7 @@ ButtonsWindow::ButtonsWindow(QWidget *parent) : QWidget(parent) {
     });
     lockOnButton->setFixedWidth(150);
     lockOnButton->setFixedHeight(150);
-    lockOnButton->setWindowOpacity(all_opac);
+    //lockOnButton->setWindowOpacity(all_opac);
     btns_layoutL->addSpacing(15);
     btns_layoutL->addWidget(lockOnButton);
     lockOnButton->setStyleSheet(QString(btn_style).arg(mButtonColors.at(mLockOnButton)));
@@ -270,7 +270,7 @@ ButtonsWindow::ButtonsWindow(QWidget *parent) : QWidget(parent) {
     });
     accelCtrlButton->setFixedWidth(150);
     accelCtrlButton->setFixedHeight(150);
-    accelCtrlButton->setWindowOpacity(all_opac);
+    //accelCtrlButton->setWindowOpacity(all_opac);
     //btns_layout->addSpacing(10);
     btns_layout->addWidget(accelCtrlButton);
     accelCtrlButton->setStyleSheet(QString(btn_style).arg(mButtonColors.at(mAccelCtrlButton)));
@@ -285,7 +285,7 @@ ButtonsWindow::ButtonsWindow(QWidget *parent) : QWidget(parent) {
     });
     decelCtrlButton->setFixedWidth(150);
     decelCtrlButton->setFixedHeight(150);
-    decelCtrlButton->setWindowOpacity(all_opac);
+    //decelCtrlButton->setWindowOpacity(all_opac);
     btns_layout->addSpacing(15);
     btns_layout->addWidget(decelCtrlButton);
     decelCtrlButton->setStyleSheet(QString(btn_style).arg(mButtonColors.at(mDecelCtrlButton)));
@@ -305,7 +305,7 @@ ButtonsWindow::ButtonsWindow(QWidget *parent) : QWidget(parent) {
     });
     accelEngagedButton->setFixedWidth(150);
     accelEngagedButton->setFixedHeight(150);
-    accelEngagedButton->setWindowOpacity(all_opac);
+    //accelEngagedButton->setWindowOpacity(all_opac);
     btns_layout->addSpacing(15);
     btns_layout->addWidget(accelEngagedButton);
     accelEngagedButton->setStyleSheet(QString(btn_style).arg(mButtonColors.at(mAccelEngagedButton > 0)));
@@ -318,7 +318,7 @@ ButtonsWindow::ButtonsWindow(QWidget *parent) : QWidget(parent) {
 
   setStyleSheet(R"(
     QPushButton {
-      color: white;
+      color: rgba(255, 255, 255, 0.5);
       text-align: center;
       padding: 0px;
       border-width: 8px;
